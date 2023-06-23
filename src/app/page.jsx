76185c61f0f1a96e9da2c4f89d5e62259d744b8b
Samuel Torres <rilla@MacBook-Pro.home>
2023-06-23 13:35:13 -0400

    complete homepage styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Home
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.description}>Turning your idea into Reality. We bring together the teams from the global tech industry.</p>
+        <button className={styles.btn}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src="/hero.png" alt='hero' width={500} height={500}/>
+      </div>
     </div>
   )
 }
