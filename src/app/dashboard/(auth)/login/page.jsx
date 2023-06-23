6d52558cea634d257e84cef072989443183c3ea4
Samuel Torres <rilla@MacBook-Pro.home>
2023-06-23 12:49:59 -0400

    did a lot of stuff following this tutorial

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./page.module.css";
 
 const Login
  = () => {
   return (
-    <div>
+    <div className={styles.container}>
         Login
     </div>
   )
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,64 @@
+"use client"
+import Link from 'next/link'
+import React from 'react'
+import styles from "./page.module.css";
+
+const links = [
+    {
+        id: 0, 
+        title: "Home",
+        url: "/"
+    },
+    {
+        id: 1, 
+        title: "Portfolio",
+        url: "/portfolio"
+    },
+    {
+        id: 2, 
+        title: "Blogs",
+        url: "/blogs"
+    },
+    {
+        id: 3, 
+        title: "About",
+        url: "/about"
+    },
+    {
+        id: 4, 
+        title: "Contact",
+        url: "/contact"
+    },
+    {
+        id: 5, 
+        title: "Dashboard",
+        url: "/dashboard"
+    }
+]
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>
+        <Link href="/" className={styles.logo}>lamamia</Link>
+        <div className={styles.links}>
+            {links.map(link => (
+                <Link 
+                    key={link.id} 
+                    href={link.url} 
+                    className={styles.link}
+                >
+                    {link.title}
+                </Link>
+            ))}
+            <button 
+                onClick={() => console.log("logged out")}
+                className={styles.logout}
+            >
+                Logout
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
