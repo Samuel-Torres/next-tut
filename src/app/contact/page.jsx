c53239e7ba39317f24e10c19f67a3488dce5429e
Samuel Torres <rilla@MacBook-Pro.home>
2023-06-24 15:50:50 -0400

    lots of work

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import styles from "./page.module.css";
+import Image from 'next/image';
+import Button from '@/components/button/Button';
 
 const Contact = () => {
   return (
-    <div className={styles.container}>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep In Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=''
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input 
+            type="text" 
+            placeholder='name'
+            className={styles.input}
+          />
+          <input 
+            type="text" 
+            placeholder='email'
+            className={styles.input}
+          />
+          <textarea 
+            name="message" 
+            id="" 
+            cols="30" 
+            rows="10"
+            className={styles.textArea}
+          />
+          <Button
+            url="#"
+            text="Send"
+            className='button'
+          />
+        </form>
+
+      </div>
+    </div>
   )
 }
 
--- src/app/portfolio/[categoryName]/page.jsx
+++ src/app/portfolio/[categoryName]/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from "./page.module.css";
+import Button from '@/components/button/Button';
+import Image from 'next/image';
 
-const Category = () => {
+const Category = ({ params }) => {
+  console.log("param:", params)
   return (
-    <div className={styles.container}>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.categoryName}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className=''>Test</h1>
+          <p className=''>Text</p>
+          <Button 
+            text="See More"
+            url="#"
+          />
+        </div>
+        <div className={styles.imageContainer}></div>
+          <Image
+            src=""
+            alt=""
+          />
+      </div>
+    </div>
   )
 }
 
