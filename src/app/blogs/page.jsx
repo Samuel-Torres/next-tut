5830856137b6a753e49e4a2b4fa4900f13f03296
Samuel Torres <rilla@MacBook-Pro.home>
2023-06-24 18:09:29 -0400

    blog work and dark mode toggle

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import styles from "./page.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Blogs = () => {
   return (
-    <div className={styles.container}>Blogs</div>
+    <div className={styles.mainContainer}>
+      <Link href="/blogs/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src=""
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+
+      <Link href="/blogs/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src=""
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
