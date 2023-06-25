6b89581871c3e482691ce2fce4f926f8fa9b3149
Samuel Torres <rilla@MacBook-Pro.home>
2023-06-24 20:18:00 -0400

    Learned fetching methods using client side, serverside, and swr

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -3,44 +3,43 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-// async function getData(id) {
-//   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-//     cache: "no-store",
-//   });
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
 
-//   if (!res.ok) {
-//     return notFound()
-//   }
+  if (!res.ok) {
+    return notFound()
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
 
-// export async function generateMetadata({ params }) {
+export async function generateMetadata({ params }) {
 
-//   const post = await getData(params.id)
-//   return {
-//     title: post.title,
-//     description: post.desc,
-//   };
-// }
+  const post = await getData(params.id)
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
 
 const BlogPost = async ({ params }) => {
-  // const data = await getData(params.id);
+  const data = await getData(params.id);
+  console.log("SINGLE BLOG POST: ", data)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Test</h1>
-          {/* <h1 className={styles.title}>{data.title}</h1> */}
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            {/* {data.desc} */}
-            description
+            {data.body}
           </p>
           <div className={styles.author}>
             <Image
               // src={data.img}
-              src=""
+              src="https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
               width={40}
               height={40}
@@ -53,7 +52,7 @@ const BlogPost = async ({ params }) => {
         <div className={styles.imageContainer}>
           <Image
             // src={data.img}
-            src=""
+            src="https://images.pexels.com/photos/3635300/pexels-photo-3635300.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
             fill={true}
             className={styles.image}
@@ -63,7 +62,26 @@ const BlogPost = async ({ params }) => {
       <div className={styles.content}>
         <p className={styles.text}>
          {/* {data.content} */}
-         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+         What is Lorem Ipsum?
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <br />
+          <br />
+          Why do we use it?
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+          <br />
+          <br />
+
+
+          Where does it come from?
+          <br />
+          <br />
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+          <br />
+          <br />
+
+          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+          <br />
+          <br />
         </p>
       </div>
     </div>
